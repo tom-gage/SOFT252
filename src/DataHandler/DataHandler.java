a236add650c187efd252ca8f52fd59d6b5475e98
Tom Gage <tomgage01@gmail.com>
2019-12-26 16:14:20 +0000

    added basic classes

---
+++ src/DataHandler/DataHandler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataHandler;
+
+/**
+ *
+ * @author Tom
+ */
+import java.util.*;
+
+public class DataHandler {
+    public static ArrayList readUserData(){
+        ArrayList usersArray = null;
+        return usersArray;
+    }
+    
+    public static void writeUserData(ArrayList UsersArray){
+        
+    }
+}
